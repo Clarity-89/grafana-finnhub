12b755c381c17a9c0fa083b3119790f03f1806d5
Clarity-89 <homes89@ukr.net>
2020-02-10 19:04:55 +0200

    Tweak ConfigEditor

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -1,35 +1,21 @@
-import React, { PureComponent, ChangeEvent } from 'react';
-import { SecretFormField, FormField } from '@grafana/ui';
+import React, { FC, ChangeEvent } from 'react';
+import { FormField } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
-import { MyDataSourceOptions, MySecureJsonData } from '../types';
+import { MyDataSourceOptions } from '../types';
 
 interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> {}
 
-interface State {}
-
-export class ConfigEditor extends PureComponent<Props, State> {
-  onPathChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onOptionsChange, options } = this.props;
-    const jsonData = {
-      ...options.jsonData,
-      path: event.target.value,
-    };
-    onOptionsChange({ ...options, jsonData });
-  };
-
-  // Secure field (only sent to the backend)
-  onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onOptionsChange, options } = this.props;
+export const ConfigEditor: FC<Props> = ({ options, onOptionsChange, ...props }) => {
+  const onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
     onOptionsChange({
       ...options,
-      secureJsonData: {
-        apiKey: event.target.value,
+      jsonData: {
+        apiToken: event.target.value,
       },
     });
   };
 
-  onResetAPIKey = () => {
-    const { onOptionsChange, options } = this.props;
+  const onResetAPIKey = () => {
     onOptionsChange({
       ...options,
       secureJsonFields: {
@@ -43,39 +29,23 @@ export class ConfigEditor extends PureComponent<Props, State> {
     });
   };
 
-  render() {
-    const { options } = this.props;
-    const { jsonData, secureJsonFields } = options;
-    const secureJsonData = (options.secureJsonData || {}) as MySecureJsonData;
+  const { jsonData } = options;
 
-    return (
-      <div className="gf-form-group">
+  return (
+    <div className="gf-form-group">
+      <div className="gf-form-inline">
         <div className="gf-form">
           <FormField
-            label="Path"
+            value={jsonData.apiToken || ''}
+            label="API Token"
+            placeholder="Token for the Finnhub API"
             labelWidth={6}
             inputWidth={20}
-            onChange={this.onPathChange}
-            value={jsonData.path || ''}
-            placeholder="json field returned to frontend"
+            onReset={onResetAPIKey}
+            onChange={onAPIKeyChange}
           />
         </div>
-
-        <div className="gf-form-inline">
-          <div className="gf-form">
-            <SecretFormField
-              isConfigured={(secureJsonFields && secureJsonFields.apiKey) as boolean}
-              value={secureJsonData.apiKey || ''}
-              label="API Key"
-              placeholder="secure json field (backend only)"
-              labelWidth={6}
-              inputWidth={20}
-              onReset={this.onResetAPIKey}
-              onChange={this.onAPIKeyChange}
-            />
-          </div>
-        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
