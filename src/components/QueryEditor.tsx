0d1210b5b1e4bbed48edaf394588de0ec8dc6912
Clarity-89 <homes89@ukr.net>
2020-02-12 18:26:51 +0200

    Add data type dropdown

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -1,32 +1,35 @@
-import React, { PureComponent, ChangeEvent } from 'react';
-import { FormField } from '@grafana/ui';
+import React, { FC, ChangeEvent } from 'react';
+import { FormField, FormLabel, Segment } from '@grafana/ui';
 import { ExploreQueryFieldProps } from '@grafana/data';
 import { DataSource } from '../DataSource';
 import { MyQuery, MyDataSourceOptions } from '../types';
 
 type Props = ExploreQueryFieldProps<DataSource, MyQuery, MyDataSourceOptions>;
 
-interface State {}
-
-export class QueryEditor extends PureComponent<Props, State> {
-  onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onChange, query } = this.props;
+export const QueryEditor: FC<Props> = ({ onChange, query }) => {
+  const onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
     onChange({ ...query, queryText: event.target.value });
   };
 
-  onConstantChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onChange, query } = this.props;
+  const onConstantChange = (event: ChangeEvent<HTMLInputElement>) => {
     onChange({ ...query, symbol: event.target.value });
   };
 
-  render() {
-    const { queryText, symbol } = this.props.query;
+  const onTypeChange = (value: string) => {
+    //onChange({ ...query, namespace: value });
+  };
 
-    return (
-      <div className="gf-form">
-        <FormField width={4} value={symbol || ''} onChange={this.onConstantChange} label="Symbol" />
-        <FormField labelWidth={8} value={queryText || ''} onChange={this.onQueryTextChange} label="Query Text" tooltip="Not used yet" />
-      </div>
-    );
-  }
-}
+  const dataTypes = ['profile', 'quote', 'exchange'].map(type => ({ label: type.toUpperCase(), value: type }));
+  const { queryText, symbol } = query;
+
+  return (
+    <div className="gf-form">
+      <>
+        <FormLabel>Data type</FormLabel>
+        <Segment onChange={onTypeChange} options={dataTypes} value="profile" />
+      </>
+      <FormField width={4} value={symbol || ''} onChange={onConstantChange} label="Symbol" />
+      <FormField labelWidth={8} value={queryText || ''} onChange={onQueryTextChange} label="Query Text" tooltip="Not used yet" />
+    </div>
+  );
+};
