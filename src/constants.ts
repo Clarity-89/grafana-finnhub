30a8d77e5063feddd11003e44021f6c92b11f8af
Clarity-89 <homes89@ukr.net>
2020-02-14 10:48:43 +0200

    Add 'candle' type

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const TIMESERIES_QUERY_TYPES = ['quote', 'exchange', 'earnings'];
+export const TIMESERIES_QUERY_TYPES = ['quote', 'exchange', 'earnings', 'candle'];
 export const TABLE_QUERY_TYPES = ['profile', 'metric'];
