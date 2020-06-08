94b891eaa03c7f50522959774ec48253eaf56e2a
Clarity-89 <homes89@ukr.net>
2020-06-08 14:10:45 +0300

    Change field order

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,8 @@ export const TABLE_QUERY_TYPES = ['profile2', 'exchange', 'metric'];
 export const stockMetrics = ['price', 'valuation', 'growth', 'margin', 'management', 'financialStrength', 'perShare'];
 export const candleFields = new Map([
   ['open', 'Opening price'],
-  ['close', 'Closing price'],
   ['high', 'High price'],
   ['low', 'Low price'],
+  ['close', 'Closing price'],
   ['volume', 'Traded volume'],
 ]);
