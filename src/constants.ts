0df3bf8675b8ec36c57c92484abed8fcdd4648cf
Clarity-89 <homes89@ukr.net>
2020-07-09 10:20:42 +0300

    Remove Exchange endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const TIMESERIES_QUERY_TYPES = ['quote', 'earnings', 'candle', 'trades'];
-export const TABLE_QUERY_TYPES = ['profile2', 'exchange', 'metric'];
+export const TABLE_QUERY_TYPES = ['profile2', 'metric'];
 
 export const stockMetrics = ['price', 'valuation', 'growth', 'margin', 'management', 'financialStrength', 'perShare'];
 export const candleFields = new Map([
