20603e3c8670ad70d97b609929ea6cce8dae56e8
Clarity-89 <homes89@ukr.net>
2022-03-09 14:08:48 +0200

    Publish dist folder

--- src/module.ts
+++
@@ -1,9 +0,0 @@
-import { DataSourcePlugin } from '@grafana/data';
-import { DataSource } from './DataSource';
-import { ConfigEditor, QueryEditor } from './components';
-import { MyQuery, MyDataSourceOptions } from './types';
-
-export const plugin = new DataSourcePlugin<DataSource, MyQuery, MyDataSourceOptions>(DataSource)
-  .setConfigEditor(ConfigEditor)
-  .setQueryEditor(QueryEditor)
-  .setExploreQueryField(QueryEditor);
